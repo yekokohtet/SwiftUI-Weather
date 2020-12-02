2b88c1990d0749c9fa13673fb0e21c8c4a06fd6a
Ye Ko <yekokohtet69@gmail.com>
2020-12-03 02:25:22 +0630

    - swiftui weather

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Ye Ko on 02/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
